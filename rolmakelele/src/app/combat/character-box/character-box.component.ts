258600057e6189c5460ca8be71096fba14143fcc
Manuel Gonzalez <62593980+manuegp@users.noreply.github.com>
2025-06-12 21:54:43 +0200

    Use Bootstrap tooltip for character stats

--- rolmakelele/src/app/combat/character-box/character-box.component.ts
+++ rolmakelele/src/app/combat/character-box/character-box.component.ts
@@ -1,17 +1,19 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ElementRef, AfterViewInit, OnChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CharacterState, Stats } from '../../models/game.types';
 import { LABELS_MAP } from '../../constants/stats.map';
-import { MatTooltipModule } from '@angular/material/tooltip';
+import Tooltip from 'bootstrap/js/dist/tooltip';
 
 @Component({
   selector: 'app-character-box',
   standalone: true,
-  imports: [CommonModule, MatTooltipModule],
+  imports: [CommonModule],
   templateUrl: './character-box.component.html',
   styleUrl: './character-box.component.scss'
 })
-export class CharacterBoxComponent {
+export class CharacterBoxComponent implements AfterViewInit, OnChanges {
+  constructor(private elementRef: ElementRef) {}
+
   @Input() character: CharacterState | null = null;
   @Input() selectable = false;
   @Output() selected = new EventEmitter<void>();
@@ -29,11 +31,27 @@ export class CharacterBoxComponent {
 
   readonly statLabels = LABELS_MAP;
 
+  tooltip?: Tooltip;
+
   get healthPercent(): number {
     if (!this.character) {
       return 0;
     }
     return (this.character.currentHealth / this.character.stats.health) * 100;
+  }
+
+  get tooltipContent(): string {
+    if (!this.character) {
+      return '';
+    }
+    return this.statKeys
+      .map(key => {
+        const color = this.getStatColor(key);
+        const label = this.statLabels[key];
+        const value = this.getCurrentStat(key);
+        return `<div><span style="color:${color}">${label}: ${value}</span></div>`;
+      })
+      .join('');
   }
 
   getCurrentStat(key: keyof Stats): number {
@@ -52,6 +70,20 @@ export class CharacterBoxComponent {
       return 'blue';
     }
     return 'inherit';
+  }
+
+  ngAfterViewInit(): void {
+    const el = this.elementRef.nativeElement.querySelector('.character-box');
+    if (el) {
+      this.tooltip = Tooltip.getOrCreateInstance(el, { html: true });
+      this.tooltip.setContent({ '.tooltip-inner': this.tooltipContent });
+    }
+  }
+
+  ngOnChanges(): void {
+    if (this.tooltip) {
+      this.tooltip.setContent({ '.tooltip-inner': this.tooltipContent });
+    }
   }
 
   onClick() {
